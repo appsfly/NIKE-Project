2ce004b9bf0ad637dd85b57122a558e69cef4407
DM-ss <mdarlingafrane@gamil.com>
2024-08-27 20:09:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let age = 50
+var something = true
+let code = "string"
+
+
+
+function getname(param1, param2, param3) {
+    
+   let sum = param1 + param2 + param3
+
+   return sum
+}
+
+getname()
